787df8fe905e273a6c1399ee4f24fc15b563b58b
devloberto <devloberto@mail.de>
2025-09-13 19:21:41 +0200

    fix initial script to be sourced for installation
    - with bash and zsh compatability

--- shell-dev-tools.sh
+++ shell-dev-tools.sh
@@ -1,1 +1,4 @@
-source ./src/git_delete_all_branches_but_default.sh
+SOURCE="${BASH_SOURCE[0]:-${(%):-%x}}" # bash and zsh compatability
+SCRIPT_DIR="$(realpath -e -- "$(dirname -- "$SOURCE")")"
+
+source "$SCRIPT_DIR/src/git_delete_all_branches_but_default.sh"
